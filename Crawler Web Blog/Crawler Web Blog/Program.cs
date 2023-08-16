73024eba47ea6dfadccac97eb559eb28b335fa69
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-08-17 00:11:50 +0800

    up

---
+++ Crawler Web Blog/Crawler Web Blog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace Crawler_Web_Blog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string targeturl = "https://example.com";
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void CrawlWebsite(string url)
+        {
+            WebClient webClient = new WebClient();
+            string htmlContent = webClient.DownloadString(url);
+            
+            
+        }
+    }
+}
--- ModelValidation/ModelValidation/Models/Person.cs
+++ ModelValidation/ModelValidation/Models/Person.cs
@@ -5,12 +5,21 @@ namespace ModelValidation.Models
     public class Person
     {
         [Required(ErrorMessage = "{0} can't be empty or null")]
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between" +
+                                                            "{2} and {1} characters longs")]
+        [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} should contain only alphabets, " +
+                                                          "space and dot (.)") ]
         public string? PersonName { get; set; }
+
         [Required]
+        [EmailAddress(ErrorMessage = "{0} should be a proper email address." )]
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage =  "{0} should be between ${1} and ${2}")]
         public double? Price { get; set; }
 
         public override string ToString()
