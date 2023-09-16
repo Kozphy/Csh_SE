f1f1886c2825ee4ee159d32fc8b9ed20a8d2abc2
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-09-17 00:11:52 +0800

    up

--- Thead_test/Thead_test/Program.cs
+++ Thead_test/Thead_test/Program.cs
@@ -1,18 +1,77 @@
-﻿namespace Thead_test
+﻿namespace Thead_task_test
 {
     internal class Program
     {
+
+        public static readonly List<Thread> ThreadList = new List<Thread>();
+
+        // TODO: fix run concurrently
         static void Main(string[] args)
         {
+
+        }
+
+        private static void ThreadStart()
+        {
+            // Task
+            Task task = new Task(() =>
+            {
+                Console.WriteLine("Running task in seperate thread...");
+                int result = AddNumbers(5, 10);
+                Console.WriteLine("Result of addition  " + result);
+            });
+            task.Start();
+
+            /* 
+             thread = an execution path of program
+                      We can use multiple threads to perform,
+                      different tasks of our program at the same time.
+                      Current thread running i "main" thread
+                      using Sysem.Threading;
+            */
+
             Thread mainThread = Thread.CurrentThread;
             mainThread.Name = "Main Thread";
+            Console.WriteLine(mainThread.Name + "Start!");
 
-            Thread thread1 = new Thread();
-            Thread thread2 = new Thread();
-            thread1.Start();
-            thread2.Start();
+            Action DCountDown = CountDown;
+            Action DCountUp = CountUp;
+
+            Thread threadCountDown = new Thread(CountDown);
+            threadCountDown.Start();
+
+            Thread threadCountUp = new Thread(CountUp);
+            threadCountUp.Start();
+
             Console.WriteLine(mainThread.Name + "is complete!");
             Console.ReadKey();
+        }
+
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void CountDown()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine("Timer #1: " + i + " seconds");
+                // sleep 1 second
+                Thread.Sleep(1000);
+            };
+
+            Console.WriteLine("Timer #1 is complete!");
+        }
+
+        public static void CountUp()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Timer #2: " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #2 is complete");
         }
     }
 }
---
+++ Thead_test/Thead_test/ThreadClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thead_task_test
+{
+    public class MXThread 
+    { 
+        public void mythr()
+        {
+            for (int j = 0; j < 2; j++) 
+            {
+                Console.WriteLine("My Thread is" + " in progress...!!");
+            }
+        }
+    }
+
+    // Driver Class
+    public class GFG 
+    {
+        // Main method
+        public static void Main() 
+        {
+            MXThread obj = new MXThread();
+
+            Thread t = new Thread(new ThreadStart(obj.mythr));
+            t.Start();
+        }
+    }
+}
