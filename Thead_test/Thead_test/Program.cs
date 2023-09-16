589243ec29c02772de705ea84e844959494c2da1
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-09-16 15:17:29 +0800

    up

---
+++ Thead_test/Thead_test/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Thead_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main Thread";
+
+            Thread thread1 = new Thread();
+            Thread thread2 = new Thread();
+            thread1.Start();
+            thread2.Start();
+            Console.WriteLine(mainThread.Name + "is complete!");
+            Console.ReadKey();
+        }
+    }
+}
