44e62c7adecd38f1468f707076faa8fd45ffcf3a
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-12-17 18:41:21 +0800

    up

--- ADO/Azure_Apps/Demo/ServerA/app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-app.listen(3000);
-
-app.use(express.static("public"));
-
-app.get("/getData", function (req, res) {
-    res.send(' {"id": 3, "qty": 100 } ')
-})
--- ADO/Azure_Apps/More/JavaScript/08_module/main.js
+++
@@ -1,5 +0,0 @@
-import Dog, { playWith as play, Animal } from "./library.js";
-
-let obj = new Dog(undefined, 3);
-console.log(obj.weight, obj.location);
-play(obj);
